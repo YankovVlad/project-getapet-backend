c534e95df5392ec193205b64e1712d906e1c8c5f
Владислав Янков <vladankov996@gmail.com>
2026-07-27 18:24:15 +0300

    attempt to deploy

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM node:20-alpine
 
 WORKDIR /app
 
-COPY . .
+COPY package*.json ./
 RUN npm install
+
+COPY . .
 RUN npx prisma generate
 
 CMD ["sh", "-c", "npm run build && prisma migrate deploy && node dist/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://project-getapet-backend-production.up.railway.app',
+    ],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 
